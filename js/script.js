5fbf21444da571ca93ddeceef7a2c3c072ab3df2
ManuLosta <manulostalo@hotmail.com.ar>
2021-03-01 23:42:42 -0300

    Nueva landing page

--- js/script.js
+++ js/script.js
@@ -2,24 +2,61 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 
 function switchTheme(e) {
   if (e.target.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark'); 
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
   }
-  else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light'); 
-  }    
 }
 
 // Check user preferences
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : null;
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
 }
 
 toggleSwitch.addEventListener("change", switchTheme, true);
+
+// Typewriter effect Code Taken from CSS
+var aText = new Array("Losta.xyz");
+
+var iSpeed = 125;
+var iIndex = 0;
+var iArrLength = aText[0].length;
+var iScrollAt = 20;
+
+var iTextPos = 0;
+var sContents = "";
+var iRow;
+var delay = 700;
+
+function typewriter() {
+  sContents = " ";
+  iRow = Math.max(0, iIndex - iScrollAt);
+  var destination = document.getElementById("typedtext");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + "<br />";
+  }
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
+  if (iTextPos++ == iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
+}
+
+setTimeout(function() {
+    typewriter();
+}, delay);
