e592e05dccce1279248a422c71363fc9d4da2e71
ManuLosta <manulostalo@hotmail.com.ar>
2021-06-05 20:31:29 -0300

    Added header background image

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 // start the site scrolled to hide the first section
 window.scrollTo(0, 200);
+
+const scrollText = document.getElementById('scroll');
+const title = document.getElementById('title');
+
+window.onscroll = e => {
+  let position = pageYOffset;
+
+  if (position <= 200) {
+    title.style.left = 0 + 'px';
+    scrollText.classList.remove('hidden');
+  } else if (position > 300) {
+    scrollText.classList.add('hidden');
+    title.style.left = -position + 300 + 'px';
+  }
+};
