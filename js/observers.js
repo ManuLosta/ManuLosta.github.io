eed72170802abd95540b432f26a6b68e20604b00
ManuLosta <manulostalo@hotmail.com.ar>
2021-06-06 16:06:35 -0300

    New skills section and description fix

--- js/observers.js
+++ js/observers.js
@@ -1,21 +1,21 @@
 const faders = document.querySelectorAll('.fade-in');
+const progressBars = document.querySelectorAll('.skills__progressbar');
 
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
       return;
     } else {
-      console.log('hola');
       entry.target.classList.add('appear');
       appearOnScroll.unobserve(entry.target);
     }
   });
 });
 
-console.log(faders);
-
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
-/* 
-appearOnScroll.observe(faders); */
+
+progressBars.forEach(progressBar => {
+  appearOnScroll.observe(progressBar);
+});
